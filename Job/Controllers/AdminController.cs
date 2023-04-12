ca8ebe73c7f6f3eded19806c4682752a33f63d43
Star <Star@5CG9492YSG>
2023-04-12 17:45:36 +0530

    Add project files.

---
+++ Job/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Job.Models;
+namespace Job.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(int Id)
+        {
+
+            Console.WriteLine(Id);
+            return View();
+        
+        }
+
+
+        [HttpPost]
+        public IActionResult Index( Login fd)
+        {
+            Console.WriteLine(fd.username);
+            Console.WriteLine(fd.password);
+
+            return View();
+        }
+
+
+    }
+}
---
+++ Job/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace Job.Models
+{
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
