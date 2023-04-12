2b77756f2114b0c0bd6e6c351f264ef8a07c7c26
Priya J <priyadj2001@gmai.com>
2023-04-12 20:58:59 +0530

    Job_Details Values inserted

--- DBscript.sql
+++ DBscript.sql
@@ -25,3 +25,40 @@ VALUES
 ( 'user8','123123', 0, 'user8@example.com', 890123, '1819 Spruce St'),
 ( 'user9','123123', 1, 'user9@example.com', 901234, '2021 Oakwood Rd'),
 ( 'user10','123123', 0, 'user10@example.com', 12345, '2223 Chestnut Ave');
+
+CREATE TABLE JOB_DETAILS (
+JOB_ID INT IDENTITY (1,1) PRIMARY KEY,
+JOB_ROLE NCHAR (20),
+SALARY_PCK FLOAT,
+CMPY_NAME NCHAR(25),
+CMPY_LOCATION NCHAR (10),
+REQ_EXP INT,
+PLACED_EMP INT
+);
+
+SELECT * FROM JOB_DETAILS;
+
+--USER INTERSETED DB
+
+CREATE TABLE JOB_IRST(
+JOB_ID INT,
+USER_ID INT
+);
+
+SELECT * FROM JOB_IRST;
+
+--Inserting values to the Job Values
+
+INSERT INTO dbo.JOB_DETAILS(JOB_ROLE,SALARY_PCK,CMPY_NAME,CMPY_LOCATION,REQ_EXP,PLACED_EMP)
+VALUES ('Jr Cloud Engineer', 1000000,'VD Corp', 'Chennai', 0 ,0 ),
+('Data Engineer', 800000,'XD Solution', 'Coimbatore', 0 ,0 ),
+('Software Engineer', 1100000,'MIN Pvt ltd', 'Mumbai', 2 ,0 ),
+('Intern', 600000,'ABC Corp', 'Coimbatore', 0 ,0 ),
+('jr Software develper', 900000,'XYZ Corp', 'Hyderabad', 0 ,0 ),
+('Software develper', 1400000,'Global Solution', 'Noida', 4 ,0 ),
+('Software Engineer', 1200000,'Delite', 'Bangalore', 3 ,0 ),
+('HR Manager', 1500000,'Global Solution', 'Chennai', 6,0 ),
+('Cloud Engineer', 1600000,'VD Corp', 'Bangalore', 3 ,0 ),
+('Intern', 600000,'Delite', 'Bangalore', 0 ,0 );
+
+
